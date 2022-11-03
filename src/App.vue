84a0787b5668ecf4ae4808291893e0154bf9b4ba
chatmongcol <cdadwngs@gmail.com>
2022-11-03 17:10:09 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <!-- MENU -->
+   <MenuComponent />
+   <SlideComponent />
+
+   <router-view />
+
+   <!-- Footer -->
+   <FooterComponent />
+
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import MenuComponent from '@/components/MenuComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+import SlideComponent from '@/components/SlideComponent.vue'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'HomeView',
+  components: {
+    MenuComponent,
+    FooterComponent,
+    SlideComponent
   }
 }
-</style>
+</script>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="card">
       <div class="card-body">
-        <h1>Home page</h1>
+        <h1>ประวัติส่วนตัว</h1>
       </div>
     </div>
   </div>
